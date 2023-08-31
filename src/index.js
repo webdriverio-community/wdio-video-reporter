7d2c03e37d42d4c61b6c076181ad6bfdc9be3543
Christian Bromann <git@bromann.dev>
2023-08-31 17:30:09 +0200

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -145,9 +145,12 @@ export default class Video extends WdioReporter {
     }
 
     // Skips screenshot if alert is displayed
-    browser.isAlertOpen().then((result) => {
-      if (!result) this.addFrame();
-      else helpers.debugLog('Skipped screenshot to avoid unexpected alert closing\n');
+    return browser.isAlertOpen().then((result) => {
+      if (!result) {
+          return this.addFrame();
+      }
+      
+      return helpers.debugLog('Skipped screenshot to avoid unexpected alert closing\n');
     })
   }
 
